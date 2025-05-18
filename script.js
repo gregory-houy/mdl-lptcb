557651eb566d165c8056bceb20a0c374144bca35
gregory.houy <houygregory2009@gmail.com>
2025-05-18 16:51:48 +0200

    liens

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const navLinks = document.querySelector('.nav-links');
+
+    if (hamburgerMenu && navLinks) {
+        hamburgerMenu.addEventListener('click', function() {
+            navLinks.classList.toggle('open');
+            hamburgerMenu.classList.toggle('open');
+        });
+    }
+});
